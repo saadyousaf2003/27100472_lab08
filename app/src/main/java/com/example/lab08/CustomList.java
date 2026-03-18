a318e835003c4941e78324818a8964939076adf5
saadyousaf2003 <saadyousaf2003@gmail.com>
2026-03-18 23:50:30 +0500

    Implement deleteCity

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -19,4 +19,5 @@ public class CustomList {
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
     public boolean hasCity(City city) { return cities.contains(city); }
+    public void deleteCity(City city) { cities.remove(city); }
 }
