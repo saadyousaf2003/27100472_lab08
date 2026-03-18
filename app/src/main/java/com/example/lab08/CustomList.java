a1bcb75a7f05ba6e0cd381bf2cb492da6bce4fd0
saadyousaf2003 <saadyousaf2003@gmail.com>
2026-03-18 23:53:48 +0500

    Implement countCities

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -20,4 +20,5 @@ public class CustomList {
     // public boolean hasCity(City city) { ... }
     public boolean hasCity(City city) { return cities.contains(city); }
     public void deleteCity(City city) { cities.remove(city); }
+    public int countCities() { return cities.size(); }
 }
